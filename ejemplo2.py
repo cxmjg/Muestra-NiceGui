1b7d72f8c1c62376714f35b61f27a8c972f08627
Marcos Garcia <mgarciaunlp@gmail.com>
2025-06-02 18:03:05 -0300

    commit inicial

---
+++ ejemplo2.py
@@ -0,0 +1,34 @@
+from nicegui import html, ui
+
+estilos = open("./estilos.css").read()
+
+ui.add_css(estilos)
+
+with ui.header().classes('items-center') as header:
+    html.h1('Sitio de prueba')
+
+with ui.row() as principal:
+    
+    with html.nav() as contenedorMenu:
+        ui.menu_item('Menu item 1')
+        ui.menu_item('Menu item 2')
+        ui.menu_item('Menu item 3')
+
+    with ui.column() as contenido:
+        html.h1('Contenido del sitio')
+        
+        with html.article() as article:
+            ui.html('<h3>Article 1</h3>')
+            html.p('Like with any other element, you can add classes, style, props, tooltips and events. One convenience is that the keyword arguments are automatically added to the elements props dictionary.')
+
+with ui.footer() as footer:
+    ui.label('Marcos Garcia 2025')
+
+
+principal.classes('flex-nowrap')
+ui.query('nav').classes('bg-blue-400 rounded-2xl p-2 w-1/6 h-[70vh]')
+contenido.classes('bg-blue-400 rounded-2xl p-2 w-5/6 h-[70vh] items-center')
+ui.query('article').classes('bg-blue-200 rounded-2xl p-2 w-1/2')
+
+
+ui.run(port=8182, host='0.0.0.0')
---
+++ index.py
@@ -0,0 +1,33 @@
+from nicegui import html, ui
+
+estilos = open("./estilos.css").read()
+
+ui.add_css(estilos)
+
+with ui.header().classes('items-center') as header:
+    with ui.button(icon='menu'):
+        with ui.menu() as menu:
+            with html.nav() as nav:
+                ui.menu_item('Menu item 1')
+                ui.menu_item('Menu item 2')
+                ui.menu_item('Menu item 3',auto_close=False)
+                ui.separator()
+                ui.menu_item('Cerrar', menu.close)
+    html.h1('Sitio de prueba')
+    
+with ui.column().classes('items-center self-center') as body:
+    with html.article() as article1:
+        ui.html('<h3>Article 1</h3>')
+        html.p('Like with any other element, you can add classes, style, props, tooltips and events. One convenience is that the keyword arguments are automatically added to the elements props dictionary.')
+    
+    with html.article() as article2:
+        ui.html('<h3>Article 2</h3>')
+        with ui.expansion('Expandir!', icon='description').classes('w-full'):
+            html.p('Like with any other element, you can add classes, style, props, tooltips and events. One convenience is that the keyword arguments are automatically added to the elements props dictionary.')
+
+with ui.footer() as footer:
+    ui.label('Marcos Garcia 2025')
+
+ui.query('article').classes('bg-blue-200 rounded-2xl p-2 w-1/2')
+
+ui.run(port=8181, host='0.0.0.0')
